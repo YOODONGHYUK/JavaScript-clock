b9e14f766ae70e1487237d91937b0fc391d6785f
YOODONGHYUK <ydh8485@gmail.com>
2020-12-01 10:46:24 +0900

    새로고침해야만 시간이 되던 것을 새로고침없이도 시간이 변경되도록 setInterval 추가 add setInterval to avoid changing the time by page refreshing

--- clock.js
+++ clock.js
@@ -6,11 +6,15 @@ function getTime() {
     const minutes = date.getMinutes();
     const hours = date.getHours();
     const seconds = date.getSeconds();
-    clockTitle.innerText = `${hours} : ${minutes} : ${seconds}`;
+    clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours} : ${
+            minutes < 10 ? `0${minutes}` : minutes} : ${
+            seconds < 10 ? `0${seconds}` : seconds}`;  //삼항연산자이용 10미만에서 앞에 0, 이상은 원래대로
+        
 }
 
 function init(){
     getTime();
+    setInterval(getTime, 1000); //반복할 인자, 시간(milliseconds)
 }
 
 init();
